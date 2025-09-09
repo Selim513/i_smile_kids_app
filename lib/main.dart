2ebe9e7e0dd8fe0d92bb903d87da66f24170a88d
Ahmed Selim <ahmedshaabanselem@gmail.com>
2025-09-09 22:39:17 +0300

    feat: Set preferred device orientations for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,8 +15,15 @@ void main() async {
   serviceLocatorSetup();
 
   Bloc.observer = SimpleBlocObserver();
-
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
   runApp(const IsmileKids());
+    // runApp(MyApp());
+  });
+
+
 }
 
 class IsmileKids extends StatelessWidget {
